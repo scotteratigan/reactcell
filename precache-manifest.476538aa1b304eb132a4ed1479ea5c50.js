7d9b6b91e6b6669c24bf030addb6945e91c393c8
ScottE <scotteratigan@gmail.com>
2019-04-08 11:05:50 -0700

    Updates

---
+++ precache-manifest.476538aa1b304eb132a4ed1479ea5c50.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "521da981ed5af9d65faa",
+    "url": "/static/css/main.8d38aaaf.chunk.css"
+  },
+  {
+    "revision": "521da981ed5af9d65faa",
+    "url": "/static/js/main.ddd86a24.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cced0e46ad2918c7de37",
+    "url": "/static/js/2.0ba81b7e.chunk.js"
+  },
+  {
+    "revision": "c1d2b6434c45773cf921f02b7804607c",
+    "url": "/index.html"
+  }
+];
